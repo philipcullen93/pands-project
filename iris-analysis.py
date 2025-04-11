691ecc6e53af80db666eca5d9925280ba1775ce8
Philip Cullen <philip.cullen93@gmail.com>
2025-04-11 21:24:47 +0100

    Imported the Iris Dataset, used the print function to check whether the dataset had been loaded

---
+++ iris-analysis.py
@@ -0,0 +1,29 @@
+# This program is designed to analyse the Iris dataset
+# There are three main tasks associated with this program
+# 1. Output a summary of each variable into a single text file
+# 2. Save a histogram of each variable into .png files
+# 3. Output a scatter plot of each pair of variables
+# Each task will be highlighted with their own section but will all be run in the same program
+
+# Setup
+# In this section, all packages and modules required for analysis
+
+# sklearn is required to import the iris dataset
+import sklearn as skl
+
+# numpy is required to analyse the dataset
+import numpy as np
+
+# matplotlib.pyplot is required to create the histograms and scatter plots
+import matplotlib.pyplot as plt
+
+# From sklearn I sppecifically need the datasets module
+from sklearn import datasets
+
+# Importing the Iris datset
+iris = datasets.load_iris()
+
+# To check if the data has been successfully imported, I print the data.
+print(iris)
+
+
